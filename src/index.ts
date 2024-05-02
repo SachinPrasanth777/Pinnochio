4b482cec3cd11d9d9d47f9d38b88a144f3474a6b
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-05-02 21:43:05 +0530

    chore: make more neat setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from "express"
-const app=express();
-const PORT=3000;
-app.listen(PORT,()=>{
-    console.log("Running on PORT 3000")
+import express from "express";
+import { config } from "dotenv";
+config();
+const app = express();
+app.listen(process.env.PORT, () => {
+  console.log(`Running on PORT ${process.env.PORT}`);
 });
