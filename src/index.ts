1457204ae8896010587d3c26523cbd7e9afa06a6
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-05-20 15:48:06 +0530

    feat: initialize server fully

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import express from "express";
+import Loaders from "./loaders";
 import config from "./config";
-const app = express();
-app.listen(config.PORT, () => {
-  console.log(`Running on PORT ${config.PORT}`);
-});
+
+async function start() {
+  const app = express();
+  await Loaders({ expressApp: app });
+  app
+    .listen(config.PORT, () => {
+      console.log(`
+      ------------ Server listening on port: ${config.PORT}------------
+    `);
+    })
+    .on("error", (err: Error) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+start();
